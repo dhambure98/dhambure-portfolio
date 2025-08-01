c5b4dcc761694ae81a4948c91e6a551cdd11d67c
Akila Dhambure Liyanage <akila789liyanage@gmail.com>
2025-08-01 16:22:46 +0530

    assets file creations

---
+++ assets/assets.js
@@ -0,0 +1,431 @@
+import user_image from './user-image.png';
+import user_image2 from './user-image2.png';
+import code_icon from './code-icon.png';
+import code_icon_dark from './code-icon-dark.png';
+import edu_icon from './edu-icon.png';
+import edu_icon_dark from './edu-icon-dark.png';
+import project_icon from './project-icon.png';
+import project_icon_dark from './project-icon-dark.png';
+import vscode from './vscode.png';
+import firebase from './firebase.png';
+import figma from './figma.png';
+import git from './git.png';
+import mongodb from './mongodb.png';
+import postman from './postman.png';
+import jira from './jira.png';
+import selenium from './selenium.png';
+import right_arrow_white from './right-arrow-white.png';
+import logo from './logo.png';
+import logo_dark from './logo_dark.png';
+import mail_icon from './mail_icon.png';
+import mail_icon_dark from './mail_icon_dark.png';
+import phone_icon from './phone_icon.png';
+import phone_icon_dark from './phone_icon_dark.png';
+import profile_img from './profile-img.png';
+import download_icon from './download-icon.png';
+import hand_icon from './hand-icon.png';
+import header_bg_color from './header-bg-color.png';
+import moon_icon from './moon_icon.png';
+import sun_icon from './sun_icon.png';
+import arrow_icon from './arrow-icon.png';
+import arrow_icon_dark from './arrow-icon-dark.png';
+import menu_black from './menu-black.png';
+import menu_white from './menu-white.png';
+import close_black from './close-black.png';
+import close_white from './close-white.png';
+import web_icon from './web-icon.png';
+import mobile_icon from './mobile-icon.png';
+import ui_icon from './ui-icon.png';
+import graphics_icon from './graphics-icon.png';
+import profomance_icon from './profomance_icon.png';
+import software_icon from './software_icon.png';
+import right_arrow from './right-arrow.png';
+import send_icon from './send-icon.png';
+import right_arrow_bold from './right-arrow-bold.png';
+import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+import telecom_logo from './telecom_logo.png';
+import coursera_logo from './coursera_logo.png';
+import google_analytics_logo from './google_analytics_logo.png';
+import sololearn_logo from './sololearn_logo.png';
+import sesoc_logo from './sesoc_logo.png';
+import ieee_logo from './ieee_logo.png';
+import cgu_logo from './cgu_logo.png';
+import seds_logo from './seds_logo.png';
+import ypsl_logo from './ypsl_logo.png';
+
+import sesoc1 from './sesoc/sesoc1.png';
+import sesoc2 from './sesoc/sesoc2.png';
+import sesoc3 from './sesoc/sesoc3.png';
+import sesoc4 from './sesoc/sesoc4.png';
+import sesoc5 from './sesoc/sesoc5.png';
+import sesoc6 from './sesoc/sesoc6.png';
+import sesoc7 from './sesoc/sesoc7.png';
+import sesoc8 from './sesoc/sesoc8.png';
+import sesoc9 from './sesoc/sesoc9.png';
+import sesoc10 from './sesoc/sesoc10.png';
+
+import ieee1 from './ieee/ieee1.png';
+import ieee2 from './ieee/ieee2.png';
+import ieee3 from './ieee/ieee3.png';
+import ieee4 from './ieee/ieee4.png';
+import ieee5 from './ieee/ieee5.png';
+import ieee6 from './ieee/ieee6.png';
+import ieee7 from './ieee/ieee7.png';
+import ieee8 from './ieee/ieee8.png';
+import ieee9 from './ieee/ieee9.png';
+
+import ypsl1 from './ypsl/ypsl1.png';
+import ypsl2 from './ypsl/ypsl2.png';
+import ypsl3 from './ypsl/ypsl3.png';
+import ypsl4 from './ypsl/ypsl4.png';
+import ypsl5 from './ypsl/ypsl5.png';
+import ypsl6 from './ypsl/ypsl6.png';
+import ypsl7 from './ypsl/ypsl7.png';
+import ypsl8 from './ypsl/ypsl8.png';
+import ypsl9 from './ypsl/ypsl9.png';
+import ypsl10 from './ypsl/ypsl10.png';
+import ypsl11 from './ypsl/ypsl11.png';
+import ypsl12 from './ypsl/ypsl12.png';
+import ypsl13 from './ypsl/ypsl13.png';
+import ypsl14 from './ypsl/ypsl14.png';
+import ypsl15 from './ypsl/ypsl15.png';
+import ypsl16 from './ypsl/ypsl16.png';
+import ypsl17 from './ypsl/ypsl17.png';
+import ypsl18 from './ypsl/ypsl18.png';
+import ypsl19 from './ypsl/ypsl19.png';
+import ypsl20 from './ypsl/ypsl20.png';
+import ypsl21 from './ypsl/ypsl21.png';
+import ypsl22 from './ypsl/ypsl22.png';
+import ypsl23 from './ypsl/ypsl23.png';
+import ypsl24 from './ypsl/ypsl24.png';
+import ypsl25 from './ypsl/ypsl25.png';
+import ypsl26 from './ypsl/ypsl26.png';
+import ypsl27 from './ypsl/ypsl27.png';
+import ypsl28 from './ypsl/ypsl28.png';
+
+import seds1 from './seds/seds1.png';
+import seds2 from './seds/seds2.png';
+import seds3 from './seds/seds3.png';
+import seds4 from './seds/seds4.png';
+import seds5 from './seds/seds5.png';
+import seds6 from './seds/seds6.png';
+import seds7 from './seds/seds7.png';
+import seds8 from './seds/seds8.png';
+import seds9 from './seds/seds9.png';
+import seds10 from './seds/seds10.png';
+import seds11 from './seds/seds11.png';
+import seds12 from './seds/seds12.png';
+import seds13 from './seds/seds13.png';
+import seds15 from './seds/seds15.png';
+import seds16 from './seds/seds16.png';
+import seds17 from './seds/seds17.png';
+import seds18 from './seds/seds18.png';
+
+import cgu1 from './cgu/cgu1.png';
+import cgu2 from './cgu/cgu2.png';
+import cgu3 from './cgu/cgu3.png';
+import cgu4 from './cgu/cgu4.png';
+import cgu5 from './cgu/cgu5.png';
+import cgu6 from './cgu/cgu6.png';
+import cgu7 from './cgu/cgu7.png';
+import cgu8 from './cgu/cgu8.png';
+import cgu9 from './cgu/cgu9.png';
+import cgu10 from './cgu/cgu10.png';
+import cgu11 from './cgu/cgu11.png';
+
+
+export const assets = {
+    user_image,
+    user_image2,
+    code_icon,
+    code_icon_dark,
+    edu_icon,
+    edu_icon_dark,
+    project_icon,
+    project_icon_dark,
+    vscode,
+    firebase,
+    figma,
+    git,
+    postman,
+    jira,
+    selenium,
+    mongodb,
+    right_arrow_white,
+    logo,
+    logo_dark,
+    mail_icon,
+    mail_icon_dark,
+    phone_icon,
+    phone_icon_dark,
+    profile_img,
+    download_icon,
+    hand_icon,
+    header_bg_color,
+    moon_icon,
+    sun_icon,
+    arrow_icon,
+    arrow_icon_dark,
+    menu_black,
+    menu_white,
+    close_black,
+    close_white,
+    web_icon,
+    mobile_icon,
+    ui_icon,
+    graphics_icon,
+    profomance_icon,
+    software_icon,
+    right_arrow,
+    send_icon,
+    right_arrow_bold,
+    right_arrow_bold_dark,
+    telecom_logo,
+    coursera_logo,
+    google_analytics_logo,
+    sololearn_logo,
+    sesoc_logo,
+    ieee_logo,
+    cgu_logo,
+    seds_logo,
+    ypsl_logo,
+    
+    sesoc_images: [
+    sesoc1, sesoc2, sesoc3, sesoc4, sesoc5, sesoc6, sesoc7, sesoc8, sesoc9, sesoc10,
+    ],
+
+    ieee_images: [
+    ieee1, ieee2, ieee3, ieee4, ieee5, ieee6, ieee7, ieee8, ieee9,
+    ],
+
+    ypsl_images: [
+    ypsl1, ypsl2, ypsl3, ypsl4, ypsl5, ypsl6, ypsl7, ypsl8, ypsl9, ypsl10, ypsl11, ypsl12, ypsl13, ypsl14, ypsl15, ypsl16, ypsl17, ypsl18, ypsl19, ypsl20, ypsl21, ypsl22, ypsl23, ypsl24, ypsl25, ypsl26, ypsl27, ypsl28,
+    ],
+
+    seds_images: [
+    seds1, seds2, seds3, seds4, seds5, seds6, seds7, seds8, seds9, seds10, seds11, seds12, seds13, seds15, seds16, seds17, seds18,
+    ],
+
+    cgu_images:[
+    cgu1, cgu2, cgu3, cgu4, cgu5, cgu6, cgu7, cgu8, cgu9, cgu10, cgu11,
+    ],
+};
+
+
+export const blogData = [
+    {
+        title: 'Introducing Next-Gen Ray-Ban Meta Smart Glasses in Partnership with Meta',
+        description: 'In a ground breaking collaboration with Essilor Luxottica , Meta ( formerly Facebook ) proudly presents the much - anticipated Ray -Ban Meta smart glasses , the next...',
+        bgImage: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiTZLYOHughx8-k8FgnGhyphenhyphengxywungQOTw5hDknSvbU5Ru5vQ5WQ2iM2mudOmfWwFGjgS4ihBvKfrVRQaP8Qc9Kyv_tU6kctgmKb9HEtjHopG3qXQVNVd2fPuF1A_vFYNihjlPVO_OZMDp1EpqiHz502gQEmkM2_NkOW6Bfpkeu0HISc2yQTHJuDHv2pZd8/s16000-rw/PoP-Lost-Crown-Announced_06-08-23.png',
+        link: 'https://www.techwadiya.us/2023/10/introducing-next-generation-ray-ban.html'
+    },
+    {
+        title: 'Emo Robot: A Fun and Educational Companion for People of All Ages',
+        description: 'Emo is a new type of desktop companion that is powered by artificial intelligence (AI). It is designed to be fun, engaging, and educational. Emo can learn and grow over time, and it...',
+        bgImage: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5Z2F5TKPOf2FBmmhCQRlm47G4Uj1bJHrrVm8CpelJ2-kwDZGjvSNmfhz9C5S4l5swE-P4fHl-0_wmurQciFqnoYRMC_8rGhlFSLL8qBwZ5G26sW9nYHE9SPbX05IvIZqjl7rEKsvAxJNpLn3mraVfjQ9IFMIEkgjUD8Q0aLAk1JrDCM6Pst1vgAjaZDQ/s16000-rw/emo.jpg',
+        link: 'https://www.techwadiya.us/2023/09/emo-robot-fun-and-educational-companion.html'
+    },
+    {
+        title: "Google's 25th Birthday: A Time to Thank the Company for All It Has Done",
+        description: 'On September 27, 1998, two Stanford graduate students, Sergey Brin and Larry Page, founded Google Inc. in a rented garage in Menlo Park, California. Their goal was to create...',
+        bgImage: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjGqoyxIqP3vORSs2A6Wen8lR9mff_Ut174w969487zsl8jgAwKZzHEoo1imgmFfeTrgEsN1oAR5CQgUdpCGSFKwLrA_H1Y6dtBZmjd8HV-NtQ2m6PVX6Irn-tzwBFhjeUoHnBXkpO2F-kckHqsqr8Okm76Vburr2Hz3as4uOlG7BmOsEzrimrzpzYDjk4/s16000-rw/google.jpg',
+        link: 'https://www.techwadiya.us/2023/09/google-turns-25-quarter-century-of.html'
+    }
+]
+
+export const workData = [
+    {
+        title: 'Akira Global Group',
+        category: 'Web Design',
+        bgImage: '/work-1.png',
+        link: 'https://akiraglobalgroup.com/'
+    },
+    {
+        title: 'Kina Rain',
+        category: 'Web Design',
+        bgImage: '/work-2.png',
+        link: 'https://kinarain.com/'
+    },
+    {
+        title: 'Sound Theraphy by FORTUDE',
+        category: 'Web Design',
+        bgImage: '/work-3.png',
+        link: 'https://shutterbugs.fortude.co/'
+    },
+    {
+        title: 'CeylonX',
+        category: 'Web Design',
+        bgImage: '/work-4.png',
+        link: 'https://ceylon-x.netlify.app/'
+    },
+    {
+        title: 'Apollo Car Service',
+        category: 'Web Design',
+        bgImage: '/work-5.png',
+        link: 'https://lawo69.github.io/Apollo-Car-Service/'
+    },
+    {
+        title: 'OBUDDY',
+        category: 'Web Design',
+        bgImage: '/work-6.png',
+        link: 'https://lawo69.github.io/OBUDDY-App-Website/'
+    },
+    {
+        title: 'University of SriLanka',
+        category: 'Web Design',
+        bgImage: '/work-7.png',
+        link: 'https://lawo69.github.io/University-of-Srilanka/'
+    },
+    {
+        title: 'PIXMATO',
+        category: 'Web Design',
+        bgImage: '/work-8.png',
+        link: 'https://pixmato.netlify.app/'
+    },
+    {
+        title: 'OLO',
+        category: 'Web Design',
+        bgImage: '/work-9.png',
+        link: 'https://www.olo.ae/'
+    },
+    {
+        title: 'Lucky',
+        category: 'Web Design',
+        bgImage: '/work-10.png',
+        link: 'https://lawo69.github.io/Luckey/'
+    },
+    {
+        title: 'Tech Mentor',
+        category: 'Web Design',
+        bgImage: '/work-11.png',
+        link: 'https://lawo69.github.io/Tech-Mentor/'
+    },
+    {
+        title: 'OBUDDY',
+        category: 'Mobile Application',
+        bgImage: '/work-12.png',
+        link: ''
+    },
+    {
+        title: 'Coffeemate',
+        category: 'Mobile Application',
+        bgImage: '/work-13.png',
+        link: ''
+    },
+    {
+        title: 'Hades',
+        category: 'Mobile Application',
+        bgImage: '/work-14.png',
+        link: 'https://drive.google.com/file/d/1L0GjOki85gcibJHHpFZ5yujG7qZkHwAN/view?usp=sharing'
+    },
+    {
+        title: 'HRIS',
+        category: 'Mobile Application',
+        bgImage: '/work-15.png',
+        link: 'https://lawo69.github.io/OBUDDY-App-Website/'
+    },
+    {
+        title: 'MedTalk',
+        category: 'Mobile Application',
+        bgImage: '/work-16.png',
+        link: ''
+    },
+    {
+        title: 'Savour Street POS',
+        category: 'ERP',
+        bgImage: '/work-17.png',
+        link: 'https://savourpos.ceylonxcorporation.com/'
+    },
+    {
+        title: 'Sanrom Pharmacy POS',
+        category: 'ERP',
+        bgImage: '/work-18.png',
+        link: 'https://pharmacypos.ceylonx.com/'
+    },
+    {
+        title: 'HRIS HR System',
+        category: 'ERP',
+        bgImage: '/work-19.png',
+        link: 'https://ceylonxhr.netlify.app/dashboard'
+    },
+    {
+        title: 'Exchange Rate Cal',
+        category: 'Tools',
+        bgImage: '/work-20.png',
+        link: 'https://lawo69.github.io/Exchange-Rate-Calculator/'
+    },
+    {
+        title: 'To-Do',
+        category: 'Tools',
+        bgImage: '/work-21.png',
+        link: 'https://lawo69.github.io/To-Do/'
+    },
+    {
+        title: 'GPA Calculator',
+        category: 'Tools',
+        bgImage: '/work-22.png',
+        link: 'https://lawo69.github.io/GPA-Calculator/'
+    },
+    {
+        title: 'Resto UI/UX',
+        category: 'Design',
+        bgImage: '/work-23.png',
+        link: 'https://www.behance.net/gallery/171991871/Resto-App-UI-Food-Ordering-Application'
+    },
+    {
+        title: 'INTECSSL UI/UX',
+        category: 'Design',
+        bgImage: '/work-24.png',
+        link: 'https://www.behance.net/gallery/195685375/Construction-Company-Web-UIUX-Design'
+    },
+    {
+        title: 'Akira UI/UX',
+        category: 'Design',
+        bgImage: '/work-25.png',
+        link: 'https://www.behance.net/gallery/212100015/Mother-Company-Website'
+    },
+    {
+        title: 'eTEC',
+        category: 'Web Design',
+        bgImage: '/work-26.png',
+        link: 'https://etvec.netlify.app/'
+    }
+]
+
+export const serviceData = [
+      {
+        icon: assets.web_icon,
+        title: "Web Development",
+        fullDesc: "Web development focuses on building functional, high-performing websites and web apps. It includes both front-end and back-end development using technologies like HTML, CSS, JavaScript, React, Node.js, and more."
+      },
+      {
+        icon: assets.profomance_icon,
+        title: "Software Testing",
+        fullDesc: "Software testing ensures applications are bug-free and meet requirements. It involves manual and automated testing to verify functionality, usability, and stability before release."
+      },
+      {
+        icon: assets.ui_icon,
+        title: "UI/UX design",
+        fullDesc: "UI/UX design is centered around understanding user behavior and crafting interfaces that enhance usability. It includes wireframing, prototyping, user research, and testing to ensure intuitive interactions and visually appealing designs."
+      },
+      {
+        icon: assets.graphics_icon,
+        title: "Graphics design",
+        fullDesc: "Graphic design involves creating visually engaging content for branding, marketing, and advertising. This includes logo creation, typography, illustration, and multimedia design to communicate ideas effectively."
+      },
+      {
+        icon: assets.software_icon,
+        title: "Performance Testing",
+        fullDesc: "Graphic design involves creating visually engaging content for branding, marketing, and advertising. This includes logo creation, typography, illustration, and multimedia design to communicate ideas effectively."
+      }
+]
+
+export const infoList = [
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science' },
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
+];
+
+export const toolsData = [
+    assets.vscode, assets.firebase, assets.mongodb, assets.figma, assets.git, assets.postman, assets.jira, assets.selenium
+];
